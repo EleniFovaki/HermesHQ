a0a0dc0dbf8b5d83f25c71b2ff5ecea47b49bd0d
unknown <fovakieleni@gmail.com>
2024-01-19 00:07:32 +0200

    Login Functionality Added
    connection strings
    user class for login

--- HermeshqApi/src/main/java/com/HermeshqApi/User.java
+++ HermeshqApi/src/main/java/com/HermeshqApi/User.java
@@ -1,4 +1,34 @@
 package com.HermeshqApi;
 
 public class User {
+    private long userID;
+    public String Name;
+    private String Email;
+    private String Password;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        this.Password = Password;
+    }
 }
+
+
+
