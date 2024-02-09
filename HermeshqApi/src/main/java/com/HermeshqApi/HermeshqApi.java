56e2a1bd4934d5f03a3c3c92c8df2d889939fdf8
unknown <fovakieleni@gmail.com>
2024-02-09 17:07:06 +0200

    remove imports
    remove of unnecessary lines of import

--- HermeshqApi/src/main/java/com/HermeshqApi/HermeshqApi.java
+++ HermeshqApi/src/main/java/com/HermeshqApi/HermeshqApi.java
@@ -3,13 +3,9 @@ package com.HermeshqApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.HermeshqApi.models")
-@EnableJpaRepositories(basePackages = "com.HermeshqApi.repository")
-@ComponentScan(basePackages = "com.HermeshqApi.services")
 public class HermeshqApi {
     public static void main(String[] args) {
         SpringApplication.run(HermeshqApi.class, args);
