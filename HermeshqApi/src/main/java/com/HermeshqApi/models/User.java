cd4f90f626655c352aee9fca8c6082534698e6cd
unknown <fovakieleni@gmail.com>
2024-02-14 02:47:13 +0200

    attempt to set up db connection and jpa hibernate
    attempt to set up db connection and jpa hibernate

--- HermeshqApi/src/main/java/com/HermeshqApi/models/User.java
+++ HermeshqApi/src/main/java/com/HermeshqApi/models/User.java
@@ -1,12 +1,10 @@
 package com.HermeshqApi.models;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name="STUDENT")
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userID;
